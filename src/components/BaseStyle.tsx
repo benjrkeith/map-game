c6212a940c4a04966d4b94cc9a63385bc60024f8
benjrkeith <37029865+benjrkeith@users.noreply.github.com>
2025-11-14 13:56:54 +0000

    Fix end screen centering on large screens

--- src/components/BaseStyle.tsx
+++ src/components/BaseStyle.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react'
 export function BaseStyle({ children }: PropsWithChildren) {
   return (
     <div className="h-full w-full overflow-y-scroll bg-zinc-700">
-      <div className="mx-auto flex h-fit min-h-full w-full max-w-2xl min-w-[375px] flex-col bg-zinc-800 text-white shadow-xl shadow-black">
+      <div className="relative mx-auto flex h-fit min-h-full w-full max-w-2xl min-w-[375px] flex-col bg-zinc-800 text-white shadow-xl shadow-black">
         {children}
       </div>
     </div>
